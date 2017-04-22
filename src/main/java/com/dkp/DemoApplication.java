4f11544d37b5dfc0696e21efa921a5136a684009
zi Pang <zi Pang>
2017-04-22 12:57:22 +0800

    springboot demo

---
+++ src/main/java/com/dkp/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.dkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kunpeng on 2017/4/22.
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dkp/HelloController.java
@@ -0,0 +1,15 @@
+package com.dkp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kunpeng on 2017/4/22.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/index")
+    public String index(){
+        return "welcome";
+    }
+}
